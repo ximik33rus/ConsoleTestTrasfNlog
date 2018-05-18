ae29157c1886868cc114dcc8b4aa061a759cc950
golovanov_s <golovanov_s@IT-PC158>
2018-05-18 16:34:52 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+        public string Run()
+        {
+            string str = "66666";
+            return str;
+        }
+    }
+}
---
+++ ConsoleTestTrasfNlog/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using ConsoleApplication;
+
+namespace ConsoleTestTrasfNlog
+{
+    class Program
+    {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger(); ////+NLog
+
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Сечас начнем писать логи");
+       // Log.Error("Трынь");
+        Log.Info("Трынь");
+        Log.Debug("Трынь");
+        Log.Warn("Трынь");
+        Console.WriteLine("Конец логам");
+            ConsoleApplication.Program CA = new ConsoleApplication.Program();
+            string app = CA.Run();
+            Console.WriteLine(app);
+            Console.ReadKey();
+        }
+    }
+}
